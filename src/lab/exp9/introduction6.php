38cc303606c5a4d5419449a34b9ca7af0b7e5e50
mrudhvika940 <mrudhvikadamaraju@gmail.com>
2020-03-26 19:38:55 +0530

    Updated header and footer in all experiments

--- src/lab/exp9/introduction6.php
+++ src/lab/exp9/introduction6.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<script class='gtm'>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-W59SWTR');</script>
+
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>introduction5</title>
 </head>
