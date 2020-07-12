0f981237d6e7e6c1fd937ca1a746c73b225e26b5
SyedNazar178 <67257741+SyedNazar178@users.noreply.github.com>
2020-07-12 16:18:43 +0530

    goal 7

--- RDIP/Codes/Exp1.js
+++ RDIP/Codes/Exp1.js
@@ -8,6 +8,7 @@ var eng = [["John ate an apple before afternoon","before afternoon John ate an a
 ["John ate an apple so did she","she ate an apple so did John"],
 ["the teacher returned the book after she noticed the error","the teacher noticed the error after she returned the book","after the teacher returned the book she noticed the error","after the teacher noticed the error she returned the book","she returned the book after the teacher noticed the error","she noticed the error after the teacher returned the book","after she returned the book the teacher noticed the error","after she noticed the error the teacher returned the book"],
 ["I told her that I bought a book yesterday","I told her yesterday that I bought a book","yesterday I told her that I bought a book","I bought a book that I told her yesterday","I bought a book yesterday that I told her","yesterday I bought a book that I told her"]];
+
 var hin = [["राम और श्याम बाजार गयें","राम और श्याम गयें बाजार","बाजार गयें राम और श्याम","गयें बाजार राम और श्याम"],
 ["राम सोया और श्याम भी","श्याम सोया और राम भी","सोया श्याम और राम भी","सोया राम और श्याम भी"],
 ["मैंने उसे बताया कि राम सो रहा है","मैंने उसे बताया कि सो रहा है राम","उसे मैंने बताया कि राम सो रहा है","उसे मैंने बताया कि सो रहा है राम","मैंने बताया उसे कि राम सो रहा है","मैंने बताया उसे कि सो रहा है राम","उसे बताया मैंने कि राम सो रहा है","उसे बताया मैंने कि सो रहा है राम","बताया मैंने उसे कि राम सो रहा है","बताया मैंने उसे कि सो रहा है राम","बताया उसे मैंने कि राम सो रहा है","बताया उसे मैंने कि सो रहा है राम"],
@@ -15,6 +16,12 @@ var hin = [["राम और श्याम बाजार गयें","र
 ["बिल्लियों को मारकर कुत्ता सो गया","मारकर बिल्लियों को कुत्ता सो गया","बिल्लियों को मारकर सो गया कुत्ता","मारकर बिल्लियों को सो गया कुत्ता","कुत्ता सो गया बिल्लियों को मारकर","कुत्ता सो गया मारकर बिल्लियों को","सो गया कुत्ता बिल्लियों को मारकर","सो गया कुत्ता मारकर बिल्लियों को"],
 ["एक लाल किताब वहाँ है","एक लाल किताब है वहाँ","वहाँ है एक लाल किताब","है वहाँ एक लाल किताब"],
 ["एक बड़ी सी किताब वहाँ है","एक बड़ी सी किताब है वहाँ","बड़ी सी एक किताब वहाँ है","बड़ी सी एक किताब है वहाँ","वहाँ है एक बड़ी सी किताब","वहाँ है बड़ी सी एक किताब","है वहाँ एक बड़ी सी किताब","है वहाँ बड़ी सी एक किताब"]];
+
+
+//shuffling words of the sentence
+
+var cc;
+var bc;
 function shuffle(jumbled){
 	var jumble = jumbled.split(" ");
 	var i = jumble.length, temp, randomi;
@@ -27,66 +34,83 @@ function shuffle(jumbled){
 	}
 	return jumble;
 }
+
 var finalsentence ="";
 function fs(id,value){
-
+	
 	document.getElementById("s4").innerHTML = "Formed Sentence (after selecting words):";
 	finalsentence += value + " ";
 	 document.getElementById("s5").innerHTML = finalsentence;
 	document.getElementById(id).style.display = "none";
 	document.getElementById("s6").innerHTML = "<center><button id='reform' onclick='rs()'>Re-form the sentence</button></center>"
-
+    cc++;
+    if(bc==cc){
+		document.getElementById("s7").innerHTML = "<center><button id='correctness'  onclick='cs()'>Check the correctness</button></center>"
+    }
+    
 }
 
+//reset option
 function rs(){
-
+	
 	finalsentence = "";
 	document.getElementById("s5").innerHTML = finalsentence;
 	document.getElementById("reform").innerHTML = "";
 	document.getElementById("s4").innerHTML = "";
 }
 
+
+
+
 let selection = document.getElementById("choose");
 function run(){
 if (selection.value === "english"){
                 document.getElementById("s2").innerHTML = "(select the buttons in proper order)"
                 document.getElementById("s1").innerHTML = "Form a sentence (Declarative or Interrogative or any other type) from the given words"
+            
 var r = Math.floor(Math.random()* eng.length);
 		var jumbled = eng[r][0];
 		var j = shuffle(jumbled);
-
+		bc=0;
+    cc=0;
 		var bu ="";
 		var fbu = "";
 		for(i=0;i<=j.length-1;i++){
 			val = j[i];
 			bu = "  <button id='btn"+i+"'onclick='fs(this.id,this.value)' value='"+val+"'>"+val+"</button>  ";
 			fbu +=bu;
-
+            bc++
+           
 		}
 		s3.innerHTML = fbu.trim();
+    
+                
 }
 else if (selection.value === "hindi"){
                 document.getElementById("s2").innerHTML = "(select the buttons in proper order)"
                 document.getElementById("s1").innerHTML = "Form a sentence (Declarative or Interrogative or any other type) from the given words"
-
               var r = Math.floor(Math.random()* hin.length);
 		var jumbled = hin[r][0];
 		var j = shuffle(jumbled);
-
+		 bc=0;
+    cc=0;
 		var bu ="";
 		var fbu = "";
 		for(i=0;i<=j.length-1;i++){
 			val = j[i];
 			bu = "  <button id='btn"+i+"'onclick='fs(this.id,this.value)' value='"+val+"'>"+val+"</button>  ";
 			fbu +=bu;
+            bc++;
 		}
 		s3.innerHTML = fbu.trim();
 	}
+
 else if (selection.value === "Select Language"){
                 alert("Choose any language")
                 document.getElementById("s2").innerHTML = ""
                 document.getElementById("s1").innerHTML = ""
                 document.getElementById("s3").innerHTML = ""
                 document.getElementById("s4").innerHTML = ""
+               
             }
 }
