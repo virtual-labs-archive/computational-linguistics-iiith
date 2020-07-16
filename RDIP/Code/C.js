db38fe527687dfedea78c21fdf98dad2ab342c49
ATMAKURI SRICHARAN <sricharan191999@gmail.com>
2020-07-16 18:16:24 +0530

    functionality of check correctness

--- RDIP/Code/C.js
+++ RDIP/Code/C.js
@@ -1,22 +1,25 @@
-var sentences='{"ste1":['+'{"a":"John ate an apple before afternoon","b":"before afternoon John ate an apple<br>","c":"John before afternoon ate an apple"},'+
-'{"a":"some students like to study in the night","b":"at night some students like to study"},'+
+var sentences='{"ste1":['+'{"a":"John ate an apple before afternoon","b":"before afternoon John ate an apple","c":"John before afternoon ate an apple"},'+
+'{"a":"some students like to study in the night","b":"in the night some students like to study"},'+
 '{"a":"John and Mary went to church","b":"Mary and John went to church"},'+
-'{"a":"John went to church after eating",	"b":"after eating John went to church<br>","c":"John after eating went to church"},'+
+'{"a":"John went to church after eating",	"b":"after eating John went to church","c":"John after eating went to church"},'+
 '{"a":"did he go to market","b":"he did go to market"},'+
-'{"a":"the woman who called my sister sells cosmetics","b":"the woman who sells cosmetics called my sister<br>","c":"my sister who sells cosmetics called the woman<br>","d":"my sister who called the woman sells cosmetics"},'+
+'{"a":"the woman who called my sister sells cosmetics","b":"the woman who sells cosmetics called my sister","c":"my sister who sells cosmetics called the woman","d":"my sister who called the woman sells cosmetics"},'+
 '{"a":"John goes to the library and studies","b":"John studies and goes to the library"},'+
 '{"a":"John ate an apple so did she","b":"she ate an apple so did John"},'+
-'{"a":"the teacher returned the book after she noticed the error","b":"the teacher noticed the error after she returned the book<br>","c":"after the teacher returned the book she noticed the error<br>","d":"after the teacher noticed the error she returned the book<br>","e":"she returned the book after the teacher noticed the error<br>","f":"she noticed the error after the teacher returned the book<br>","g":"after she returned the book the teacher noticed the error<br>","h":"after she noticed the error the teacher returned the book"},'+
-'{"a":"I told her that I bought a book yesterday","b":"I told her yesterday that I bought a book<br>","c":"yesterday I told her that I bought a book<br>","d":"I bought a book that I told her yesterday<br>","e":"I bought a book yesterday that I told her<br>",	"f":"yesterday I bought a book that I told her"}]}';
+'{"a":"the teacher returned the book after she noticed the error","b":"the teacher noticed the error after she returned the book","c":"after the teacher returned the book she noticed the error","d":"after the teacher noticed the error she returned the book<br>","e":"she returned the book after the teacher noticed the error","f":"she noticed the error after the teacher returned the book","g":"after she returned the book the teacher noticed the error","h":"after she noticed the error the teacher returned the book"},'+
+'{"a":"I told her that I bought a book yesterday","b":"I told her yesterday that I bought a book","c":"yesterday I told her that I bought a book","d":"I bought a book that I told her yesterday","e":"I bought a book yesterday that I told her",	"f":"yesterday I bought a book that I told her"}]}';
 var sentences2='{"ste2":['+'{"a":"राम और श्याम बाजार गयें","b":"राम और श्याम गयें बाजार","c":"बाजार गयें राम और श्याम","d":"गयें बाजार राम और श्याम"},'+
     '{"a":"राम सोया और श्याम भी","b":"श्याम सोया और राम भी","c":"सोया श्याम और राम भी","d":"सोया राम और श्याम भी"},'+
     '{"a":"मैंने उसे बताया कि राम सो रहा है", "b":"मैंने उसे बताया कि सो रहा है राम","c":"उसे मैंने बताया कि राम सो रहा है","d":"उसे मैंने बताया कि सो रहा है राम","e":"मैंने बताया उसे कि राम सो रहा है","f":"मैंने बताया उसे कि सो रहा है राम","g":"उसे बताया मैंने कि राम सो रहा है","h":"उसे बताया मैंने कि सो रहा है राम","i":"बताया मैंने उसे कि राम सो रहा है",    "j":"बताया मैंने उसे कि सो रहा है राम", "k":"बताया उसे मैंने कि राम सो रहा है",    "l":"बताया उसे मैंने कि सो रहा है राम"},'+
     '{"a":"राम खाकर सोया","b":"खाकर राम सोया","c":"राम सोया खाकर","d":"खाकर सोया राम","e":"सोया राम खाकर","f":"सोया खाकर राम"},'+     
-    '{"a":"बिल्लियों को मारकर कुत्ता सो गया","b":"मारकर बिल्लियों को कुत्ता सो गया","c":"बिल्लियों को मारकर सो गया कुत्ता","d":"मारकर बिल्लियों को सो गया कुत्ता","e":"कुत्ता सो गया बिल्लियों को मारकर","f":"कुत्ता सो गया मारकर बिल्लियों को","g":"सो गया कुत्ता बिल्लियों को मारकर","h":"सो गया कुत्ता मारकर बिल्लियों को"},'+
+    '{"a":"बिल्लियों को मारकर कुत्ता सो गया","b":"मारकर बिल्लियों को कुत्ता सो गया","c":"बिल्लियों को मारक र सो गया कुत्ता","d":"मारकर बिल्लियों को सो गया कुत्ता","e":"कुत्ता सो गया बिल्लियों को मारकर","f":"कुत्ता सो गया मारकर बिल्लियों को","g":"सो गया कुत्ता बिल्लियों को मारकर","h":"सो गया कुत्ता मारकर बिल्लियों को"},'+
     '{"a":"एक लाल किताब वहाँ है","b":"एक लाल किताब है वहाँ","c":"वहाँ है एक लाल किताब","d":"है वहाँ एक लाल किताब"},'+
     '{"a":"एक बड़ी सी किताब वहाँ है","b":"एक बड़ी सी किताब है वहाँ","c":"बड़ी सी एक किताब वहाँ है","d":"बड़ी सी एक किताब है वहाँ","e":"वहाँ है एक बड़ी सी किताब","f":"वहाँ है बड़ी सी एक किताब","g":"है वहाँ एक बड़ी सी किताब","h":"है वहाँ बड़ी सी एक किताब"}]}'
 function dis(){
+  document.getElementById("p8").innerHTML=" ";
+  document.getElementById("p7").innerHTML=" ";
   document.getElementById("p3").innerHTML=" ";
+  document.getElementById("p3").value=" ";
   document.getElementById("p5").innerHTML=" ";
   document.getElementById("p6").innerHTML=" ";
   for(i=0;i<10;i++){
@@ -27,7 +30,8 @@ function dis(){
   if(x=="English"){
     document.getElementById("p1").innerHTML="Form a sentence(Declarative or Interrogative or any other type)from the given words"
     document.getElementById("p2").innerHTML="(select the buttons in proper order)"
-    var a=Math.floor(Math.random() * 9);
+    var a=Math.floor(Math.random() * 10);
+    document.getElementById("p1").value=a;
 obj=JSON.parse(sentences);
 var b=obj.ste1[a].a;
 var c=b.split(" ");
@@ -57,7 +61,8 @@ for(var j=d;j<10;j++){
 else{
     document.getElementById("p1").innerHTML="Form a sentence(Declarative or Interrogative or any other type)from the given words"
     document.getElementById("p2").innerHTML="(select the buttons in proper order)"
-    var a=Math.floor(Math.random() * 6);
+    var a=Math.floor(Math.random() * 7);
+    document.getElementById("p1").value=a;
     obj=JSON.parse(sentences2);
     var b=obj.ste2[a].a;
     var c=b.split(" ");
@@ -89,7 +94,7 @@ function pri(i){
   var a=document.getElementById(i).value;
   var b=document.getElementById("p4").value;
   count1=count1+1;
-  document.getElementById("p3").value+=a+"&nbsp"
+  document.getElementById("p3").value+=a+" "
   document.getElementById("p3").innerHTML+=a+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp";
   document.getElementById(i).innerHTML="  ";
   document.getElementById("p5").innerHTML="<button>Re-form the sentence</button>";
@@ -98,6 +103,9 @@ function pri(i){
 }
 }
 function reform(){
+  document.getElementById("p7").innerHTML=" "
+  document.getElementById("p8").innerHTML=" "
+  document.getElementById("p3").value=" ";
   document.getElementById("p3").innerHTML=" ";
   document.getElementById("p4").innerHTML=" ";
   document.getElementById("p5").innerHTML=" ";
@@ -108,5 +116,29 @@ function reform(){
     document.getElementById(i).innerHTML="<button>"+a+"</button>"
     document.getElementById("p6").innerHTML=" ";
   }
+}   
+}
+function ccs(){
+var m=document.getElementById ("p1").value
+var n= document.getElementById("p3").value
+n=n.trim();
+var x=document.getElementById("lan").value;
+if(x=="English"){
+obj=JSON.parse(sentences);
+if(n==obj.ste1[m].a||n==obj.ste1[m].b||n==obj.ste1[m].c||n==obj.ste1[m].d||n==obj.ste1[m].e||n==obj.ste1[m].f||n==obj.ste1[m].g||n==obj.ste1[m].h){
+document.getElementById("p7").value="CORRECT ANSWER"
+}
+else{
+  document.getElementById("p8").value="WRONG ANSWER"
+}
+}
+else{
+  obj=JSON.parse(sentences2);
+if(n==obj.ste2[m].a||n==obj.ste2[m].b||n==obj.ste2[m].c||n==obj.ste2[m].d||n==obj.ste2[m].e||n==obj.ste2[m].f||n==obj.ste2[m].g||n==obj.ste2[m].h||n==obj.ste2[m].i||n==obj.ste2[m].j||n==obj.ste2[m].k||n==obj.ste2[m].l){
+  document.getElementById("p7").value="CORRECT ANSWER"
+}
+else{
+  document.getElementById("p8").value="WRONG ANSWER"
+}
 }
 }
